067b201d28bc8be2ac7f561496e8f220106265fc
unknown <ramakrishnan.ankit@gmail.com>
2015-06-09 20:37:32 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//var cookieParser = require('cookie-parser');
+//var session = require('express-session');
+var morgan= require('morgan');
+var uuid = require('node-uuid');
+console.log('Socket.io chat app running from '+__dirname);
+app.use(morgan('dev'));
+app.get('/',function(req,res){
+	var id=uuid.v4();
+	res.cookie('user-id',id,{httpOnly:false});
+	res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection',function(socket){
+	console.log('a user connected');
+	socket.on('chat message',function(msg){
+		console.log(msg["user-id"]+" message: "+msg["msg"]);
+		socket.broadcast.emit('chat message',msg["msg"]);
+	});
+	socket.on('nick',function(msg){
+		console.log("Nick for : "+msg["user-id"]+msg["msg"]);
+	});
+	socket.on('disconnect',function(){
+		console.log('  user disconnected');
+	});
+});
+
+http.listen(3000,function(){
+	console.log('Listening on port *:3000');
+});
